a7bb5978fa12c6798bf536d3e5336f4b4ffc095e
Anna Bondar <Annabondar.pm@gmail.com>
2023-02-26 21:41:41 +0200

    first_commit

---
+++ script.js
@@ -0,0 +1,13 @@
+// task #1
+const name = "Джон";
+const admin = name;
+alert(admin);
+
+// task #2
+const ourPlanetName = "Земля";
+let currentUserName = "";
+
+// task #3
+
+const value = "10" + 10;
+console.log(typeof (value));
